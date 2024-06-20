bb43cb669857b2ce4b70b194ab5db5fa71b943ae
Darshan kumar Jain <158499116+jain123darshan@users.noreply.github.com>
2024-06-20 18:41:37 +0530

    Add files via upload
    This project build for the codsoft

---
+++ codsoft_projects/Task 1(credit_cardfraud)/credit_card_fraud.py
@@ -0,0 +1,6 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import  train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrices import acuuracy_score
+import streamlit as st
